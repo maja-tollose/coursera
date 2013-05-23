f574d74a3bee8e85c971fdbb64a20787db40b1e2
Maja Tollose <github@mayaweb.dk>
2013-05-23 21:19:24 +0200

    Init nodeapp

---
+++ nodeapp/server/server.js
@@ -0,0 +1,13 @@
+var sys = require("sys");
+var http = require("http");
+
+var server = http.createServer(
+    function (request, response) {
+        response.writeHead(200, {"content-type":"text/plain"});
+        response.write("Hellow yo from AWS!");
+        response.end();
+    }
+);
+
+server.listen(8080);
+sys.puts("Server is running on 8080");
