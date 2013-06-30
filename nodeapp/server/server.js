1a86da970131a645de5ef5cf330ab4eb15acf5d7
Maja Tollose <github@mayaweb.dk>
2013-06-30 09:55:58 +0200

    new update

--- nodeapp/server/server.js
+++ nodeapp/server/server.js
@@ -4,7 +4,7 @@ var http = require("http");
 var server = http.createServer(
     function (request, response) {
         response.writeHead(200, {"content-type":"text/plain"});
-        response.write("Hellow yo from AWS! And auto deployed from GIT");
+        response.write("Hellow yo from AWS! And auto deployed from GIT!");
         response.end();
     }
 );
